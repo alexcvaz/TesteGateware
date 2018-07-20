eb133499e8f59a1bf957f342206c144f1b71ed9e
Alex Vaz - E.Desenvolvimento - BNE <alexvaz@bne-empregos.com.br>
2018-07-20 14:15:11 -0300

    Add project files.

---
+++ TesteGateware/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteGateware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
